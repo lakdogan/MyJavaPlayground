1d82f415d6db19b3ff4febe9cdb92229033cf6e9
lakdogan <levent_akdogan@live.de>
2025-05-21 11:58:27 +0200

    Add GrossbuchstabenAusgabe: druckt alle Großbuchstaben aus von A bis Z und neue Zeile am Ende

---
+++ GrossbuchstabenAusgabe/src/main/java/org/lakdogan/Main.java
@@ -0,0 +1,16 @@
+package org.lakdogan;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Alle Großbuchstaben von A bis Z: ");
+
+        for (char buchstabe = 'A'; buchstabe <= 'Z'; buchstabe++) {
+            System.out.println(buchstabe + " ");
+        }
+
+        System.out.println();
+    }
+}
